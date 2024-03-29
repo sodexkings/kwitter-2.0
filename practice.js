3c2081ade9d4a5a79b962b63f741809f7df1e1be
sodexkings <71529813+sodexkings@users.noreply.github.com>
2024-03-29 17:38:10 -0400

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,9 @@
+
+//ADD YOUR FIREBASE LINKS
+function addUser()
+{
+    user_name = document.getElementById("user_name").value;
+    firebase.database().ref("/").child(user_name).update({
+        purpose : "adding user"
+    });
+}
